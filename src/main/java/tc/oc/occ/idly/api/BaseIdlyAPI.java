37894d58d4966c6f037ced6fe15fe1e5dcd77bc7
Pugzy <pugzy@mail.com>
2022-03-13 14:49:14 +0000

    Update isPlaying check to include match state
    Signed-off-by: Pugzy <pugzy@mail.com>

--- src/main/java/tc/oc/occ/idly/api/BaseIdlyAPI.java
+++ src/main/java/tc/oc/occ/idly/api/BaseIdlyAPI.java
@@ -2,6 +2,7 @@ package tc.oc.occ.idly.api;
 
 import org.bukkit.entity.Player;
 import tc.oc.occ.idly.IdlyUtils;
+import tc.oc.pgm.api.party.Competitor;
 import tc.oc.pgm.api.player.MatchPlayer;
 
 public class BaseIdlyAPI implements IdlyAPI {
@@ -15,6 +16,6 @@ public class BaseIdlyAPI implements IdlyAPI {
   @Override
   public boolean isPlaying(Player player) {
     MatchPlayer mp = IdlyUtils.getMatchPlayer(player);
-    return mp != null && mp.isParticipating();
+    return mp != null && mp.getParty() instanceof Competitor && !mp.getMatch().isFinished();
   }
 }
